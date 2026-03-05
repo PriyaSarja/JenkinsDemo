af423de1ad5b4657edeb95a74ea1546204709107
Priyadarshani Sarja <146539142+PriyaSarja@users.noreply.github.com>
2026-03-05 12:29:17 +0530

    Fix typo in print statement message

--- Geeks.java
+++ Geeks.java
@@ -5,6 +5,6 @@ import java.util.*;
 public class Geeks {
 
     public static void main(String[] args) {
-        System.out.println("Treat Life Like an Anime");
+        System.out.println("Treate Life Like an Anime");
     }
 }
