b9c1a4aaa982172e97f90cbd8b56fe86a8d47057
Priyadarshani Sarja <146539142+PriyaSarja@users.noreply.github.com>
2026-03-05 12:28:21 +0530

    Change output message in Geeks.java

--- Geeks.java
+++ Geeks.java
@@ -5,6 +5,6 @@ import java.util.*;
 public class Geeks {
 
     public static void main(String[] args) {
-        System.out.println("All Geeks are not Nerds");
+        System.out.println("Treat Life Like an Anime");
     }
 }
